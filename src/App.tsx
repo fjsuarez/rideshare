543949cb0f3d6d14d7cf8ddac843b50cc8a9adbf
Francisco Suarez <fjsuarez@gmail.com>
2025-03-06 16:52:31 +0100

    Refactored layout for responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import React, { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import Avatar from '@mui/material/Avatar';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import Grid from '@mui/material/Grid2';
-import Paper from '@mui/material/Paper';
-import ProfilePanel from './components/ProfilePanel';
-import ListPanel from './components/ListPanel';
-import MapPanel from './components/MapPanel';
-import Box from '@mui/material/Box';
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Grid from "@mui/material/Grid2";
+import Paper from "@mui/material/Paper";
+import ProfilePanel from "./components/ProfilePanel";
+import ListPanel from "./components/ListPanel";
+import MapPanel from "./components/MapPanel";
+import Box from "@mui/material/Box";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import ListDrawer from "./components/ListDrawer";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const theme = useTheme();
+
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
 
   const handleAvatarClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -38,23 +44,21 @@ function App() {
   };
 
   return (
-    <Paper sx={{ height: '100vh' }} elevation={3}>
+    <Paper sx={{ height: "100vh" }} elevation={3}>
       <AppBar position="static">
         <Toolbar>
-          {/* Logo and Title */}
-          <img 
-            src="/assets/logo.png" 
-            alt="RideShare Logo" 
-            style={{ width: 40, height: 40, marginRight: 8 }} 
+          <img
+            src="/assets/logo.png"
+            alt="RideShare Logo"
+            style={{ width: 40, height: 40, marginRight: 8 }}
           />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Ride
-            <Box component="span" sx={{ color: '#22A9E1' }}>
+            <Box component="span" sx={{ color: "#22A9E1" }}>
               Share
             </Box>
           </Typography>
-          
-          {/* Right side: if logged in show avatar with dropdown menu else show login button */}
+
           {isLoggedIn ? (
             <>
               <IconButton color="inherit" onClick={handleAvatarClick}>
@@ -69,21 +73,34 @@ function App() {
               </Menu>
             </>
           ) : (
-            <Button color="inherit" onClick={handleLogin}>Login</Button>
+            <Button color="inherit" onClick={handleLogin}>
+              Login
+            </Button>
           )}
         </Toolbar>
       </AppBar>
 
-      <Grid container spacing={2} sx={{ height: 'calc(100% - 64px)', paddingY: 2, paddingX: 10 }}>
-        <Grid size={{xs: 12, sm: 4}}>
+      <Grid
+        container
+        spacing={{ xs: 0, md: 2 }}
+        sx={{ height: { xs: "calc(50% - 64px)", md: "calc(100% - 64px)" }, padding: { xs: 0, md: 2}, paddingX: { lg: 10 } }}
+      >
+        <Grid
+          size={{ xs: 12, md: 6, lg: 4 }}
+          sx={{ display: { xs: "none", lg: "block" } }}
+        >
           <ProfilePanel />
         </Grid>
-        <Grid size={{xs: 12, sm: 4}}>
+        <Grid
+          size={{ xs: 12, md: 6, lg: 4 }}
+          sx={{ display: { xs: "none", md: "block" } }}
+        >
           <ListPanel />
         </Grid>
-        <Grid size={{xs: 12, sm: 4}}>
+        <Grid size={{ xs: 12, md: 6, lg: 4 }}>
           <MapPanel />
         </Grid>
+        {isSmallScreen && <ListDrawer />}
       </Grid>
     </Paper>
   );
---
+++ src/components/ListDrawer.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { grey } from "@mui/material/colors";
+import Box from "@mui/material/Box";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import ListPanel from "./ListPanel";
+
+export default function ListDrawer() {
+  const [open, setOpen] = React.useState(false);
+  const [drawerBleeding, setDrawerBleeding] = React.useState(0);
+
+  React.useEffect(() => {
+    const calculateDrawerBleeding = () => {
+      setDrawerBleeding(window.innerHeight * 0.5);
+    };
+
+    calculateDrawerBleeding();
+    window.addEventListener("resize", calculateDrawerBleeding);
+
+    return () => {
+      window.removeEventListener("resize", calculateDrawerBleeding);
+    };
+  }, []);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  return (
+    <Box sx={{ height: "100%" }}>
+      <SwipeableDrawer
+        anchor="bottom"
+        open={open}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+        swipeAreaWidth={drawerBleeding}
+        disableSwipeToOpen={false}
+        keepMounted
+        slotProps={{
+          paper: {
+            sx: {
+              height: `calc(100% - ${drawerBleeding}px)`,
+              overflow: "visible",
+            },
+          },
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            height: "100%",
+            top: -drawerBleeding,
+            borderTopLeftRadius: 8,
+            borderTopRightRadius: 8,
+            visibility: "visible",
+            right: 0,
+            left: 0,
+            p: 2,
+            backgroundColor: "#fff",
+          }}
+        >
+          <Box
+            sx={{
+              width: 30,
+              height: 6,
+              backgroundColor: grey[300],
+              borderRadius: 3,
+              position: "absolute",
+              top: 8,
+              left: "calc(50% - 15px)",
+            }}
+          />
+          <ListPanel />
+        </Box>
+      </SwipeableDrawer>
+    </Box>
+  );
+}
